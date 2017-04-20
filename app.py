929df848ebc75ffbccafe1a9bdc4da0d48e3e519
Albert Vinyés <albert.vines@gmail.com>
2017-04-20 17:44:23 +0200

    Keeping max bandwidth instead of the average

--- app.py
+++ app.py
@@ -1,33 +1,48 @@
-from subprocess import check_output
+from pymongo import MongoClient
+import datetime
 import re
+import subprocess
 import sys
 
-def network_test():
-    r = check_output(["speedtest-cli", "--simple"]).split("\n")
+client = MongoClient('localhost', 27017)
+
+def latency_test():
+    n = "20"
+    host = "8.8.8.8"
+    ps = subprocess.Popen(('ping', '-i', '0.2','-c', n, host), stdout=subprocess.PIPE)
+    output = subprocess.check_output(("awk", "-F", "/", "END {print $5}"), stdin=ps.stdout)
+    ps.stdout.close()
+    return output
+
+def bandwidth_test():
+    r = subprocess.check_output(["speedtest-cli", "--simple"]).split("\n")
     l = [None] * 3
     l[0] = r[1].split(":")[1][1:]
     l[1] = r[2].split(":")[1][1:]
     l[2] = r[0].split(":")[1][1:]
-    print(l)
     return l
 
 def main():
-    avg_down = 0
-    avg_up = 0
-    avg_latency = 0
     n = int(sys.argv[1])
-    print(n)
-    print(range(n))
+    max_down = max_up = avg_latency = 0
     for index in range(n):
-        run = network_test()
+        print "Running test number", index+1, "..."
+        run = bandwidth_test()
         nums = re.findall(r'([\d.]+)', str(run))
-        avg_down += float(nums[0])
-        avg_up += float(nums[1])
-        avg_latency += float(nums[2])
-    avg_up /= n
-    avg_down /= n
+        if (float(nums[0]) > max_down):
+            max_down = float(nums[0])
+        if (float(nums[1]) > max_up):
+            max_up = float(nums[1])
+        avg_latency += float(latency_test())
     avg_latency /= n
-    print "Download: ", str(avg_down), " Upload: ", str(avg_up), " Latency: ", str(avg_latency)
+    print "Download: ", str(max_down), " Upload: ", str(max_up), " Latency: ", str(avg_latency)
+    # TODO: Build JSON containing the date and results
+    # TODO: Store JSON in Database
+    # db = client.database
+    # hosts = db.host_collection
+    # t = hosts.update_one(
+    #                     {"email": email},
+    #                     {"$addToSet": {"hosts": {"host_name": host_name, "host_nickname": host_nickname, "notified": False}}})
 
 if __name__ == "__main__":
     main()
