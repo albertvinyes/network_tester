d73e6228dbd9df8c0fb35567c67f5a9a3f1ead70
Albert Vinyés <albert.vines@gmail.com>
2017-06-21 11:36:24 +0200

    Registering management API

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,9 @@
 from flask import Flask
 
 app = Flask(__name__)
-#app.config.from_object('config')
 
 from app.netspeed.controller import netspeed as netspeed_module
+from app.manager.controller import manager as management_module
+
 app.register_blueprint(netspeed_module)
+app.register_blueprint(management_module)
