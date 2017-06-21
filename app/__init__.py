d05203b7b42ab1e276ea8edfc41d0e83bbffcd51
Albert Vinyés <albert.vines@gmail.com>
2017-06-21 17:03:41 +0200

    Addin views module init file

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,8 @@ app = Flask(__name__)
 
 from app.netspeed.controller import netspeed as netspeed_module
 from app.manager.controller import manager as management_module
+from app.views.controller import views as views_module
 
+app.register_blueprint(views_module)
 app.register_blueprint(netspeed_module)
 app.register_blueprint(management_module)
