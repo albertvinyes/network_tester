18c070dad0d2beb9fedc549aa873dfc674fb9793
Albert Vinyés <albert.vines@gmail.com>
2017-06-21 11:37:23 +0200

    Updated test file

--- app/test.py
+++ app/test.py
@@ -2,6 +2,6 @@ from manager import controller as manager
 from netspeed import controller as netspeed
 
 manager.erase_results()
-results = netspeed.run_test(1)
+results = netspeed.run_test()
 manager.store_results(results)
 manager.print_results()
