4210353c6b8d2d66869281575b1fa80cf844ee02
Albert Vinyés <albert.vines@gmail.com>
2017-07-17 10:50:21 +0200

    Fixed high CPU usage

--- scheduler.py
+++ scheduler.py
@@ -1,13 +1,17 @@
 
 from app import test
 from app import config
+import time
 
 import schedule
 
 def job():
+    print "Running test:", time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime())
     test.run_test()
 
-schedule.every().hour.do(job)
+schedule.every(15).minutes.do(job)
+#schedule.every().hour.at(':00').do(job)
 
 while True:
     schedule.run_pending()
+    time.sleep(0.2)
