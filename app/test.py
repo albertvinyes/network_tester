81b5fdfc013ce72d3adcb56a0f13074638c54eec
Albert Vinyés <albert.vines@gmail.com>
2017-07-17 10:49:18 +0200

    Refactor

--- app/test.py
+++ app/test.py
@@ -4,7 +4,6 @@ from netspeed import controller as netspeed
 def run_test():
     results = netspeed.run_test()
     manager.store_results(results)
-    manager.print_results()
 
 if __name__ == '__main__':
     run_test()
