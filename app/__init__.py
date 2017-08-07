c33d5e34e77e3a198492f67398bff2504013f66a
albert <albert@debian.debian>
2017-08-07 11:55:49 +0200

    Gathering latency to the closer fastest SpeedTest.net server

--- app/__init__.py
+++ app/__init__.py
@@ -1,11 +1,9 @@
 from flask import Flask
-
-app = Flask(__name__)
-
-from app.controllers.netspeed.controller import netspeed as netspeed_module
+from app.controllers.analyzer.controller import analyzer as analysis_module
 from app.controllers.manager.controller import manager as management_module
 from app.controllers.views.controller import views as views_module
 
+app = Flask(__name__)
 app.register_blueprint(views_module)
-app.register_blueprint(netspeed_module)
+app.register_blueprint(analysis_module)
 app.register_blueprint(management_module)
