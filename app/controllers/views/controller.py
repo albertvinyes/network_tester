e4ff7d7a94c6988dc2d45d62fa4ebd2b341c102f
Albert Vinyés <albert.vines@gmail.com>
2017-09-07 17:19:28 +0200

    Passing server PORT to the JS

--- app/controllers/views/controller.py
+++ app/controllers/views/controller.py
@@ -1,8 +1,9 @@
 from flask import Flask, render_template
 from flask import Blueprint
+from app import config
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def main():
-    return render_template('index.html')
+    return render_template('index.html', port=config.PORT)
