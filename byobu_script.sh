cd944842e69ccf72541e458ec99271e26014c1f4
Albert Vinyés <albert.vines@gmail.com>
2017-12-04 15:49:49 +0100

    Fixing byobu script. Fixes #6

--- byobu_script.sh
+++ byobu_script.sh
@@ -1,9 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/bash
+dir_path=$(pwd)
 
 byobu new-session -d -s $USER
 
 byobu rename-window -t $USER:0 'Server'
-byobu send-keys "cd /home/pi/git/network_tester && source venv/bin/activate && python run.py" C-m
+byobu send-keys "cd "$dir_path" && source venv/bin/activate && python run.py" C-m
 
-byobu new-window -t $USER:1 -n 'OAM S'
-byobu send-keys "cd /home/pi/git/network_tester && source venv/bin/activate && python schedulery" C-m
+byobu new-window -t $USER:1 -n 'Scheduler'
+byobu send-keys "cd "$dir_path" && source venv/bin/activate && python scheduler.py" C-m
