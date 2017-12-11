5413778355a26abfcae1f3a139bb08dc9e935b20
Albert Vinyés <albert.vines@gmail.com>
2017-12-11 13:11:32 +0100

    Not updating stats when there's no connectivity

--- app/test.py
+++ app/test.py
@@ -3,10 +3,11 @@ from app.controllers.analyzer import controller as analyzer
 from app.controllers.miner import controller as miner
 
 def run_test():
+    connectivity = False
     results = analyzer.run_test()
-    print(results)
     manager.store_results(results)
-    miner.update_stats(results)
+    if "-1" not in results:
+        miner.update_stats(results)
 
 if __name__ == '__main__':
     run_test()
