17baf0338b1cfe38522cff89412394945ad84b0a
albertv <albert.vines@gmail.com>
2018-09-14 20:22:56 +0200

    Running initial test if there is no test

--- scheduler.py
+++ scheduler.py
@@ -1,9 +1,17 @@
 
+from app.controllers.manager import controller as manager
 from app import test
 from app import config
 import time
-
 import schedule
+
+results = list(manager.get_results())
+
+if len(results) == 2:
+    print('There are no tests in the Database! Running initial test...')
+    test.run_test()
+else:
+    print('Starting Scheduler! Network Tests will run automatically')
 
 def job():
     print("Running test:", time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
